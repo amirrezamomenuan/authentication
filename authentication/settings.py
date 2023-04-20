5d091a78380ad594c313c1af808f766118a43b51
reza eivazzadeh <amirrezamomenian@gmail.com>
2023-04-20 23:49:30 +0330

    added AppUser app and updated requirements.txt

--- authentication/settings.py
+++ authentication/settings.py
@@ -18,6 +18,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'AppUser',
 ]
 
 MIDDLEWARE = [
