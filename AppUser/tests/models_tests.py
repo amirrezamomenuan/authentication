e00e422bd3dcce794564d1c39fc3705476ddb230
reza eivazzadeh <amirrezamomenian@gmail.com>
2023-04-21 14:50:30 +0330

    removed a test that was not a unittest

--- AppUser/tests/models_tests.py
+++ AppUser/tests/models_tests.py
@@ -9,18 +9,6 @@ pytestmark = pytest.mark.django_db
 
 
 class TestAppUser:
-    def test_creating_users_with_duplicate_phone_numbers(self):
-        phone_number = '09123456789'
-
-        AppUser.objects.create(phone_number=phone_number)
-
-        try:
-            AppUser.objects.create(phone_number=phone_number)
-        except IntegrityError:
-            assert True
-        else:
-            assert False
-
     @pytest.mark.parametrize(
         'phone_number, creation_should_pass', [
             ('ve879o 2 92', False),
@@ -43,3 +31,8 @@ class TestAppUser:
         else:
             if not creation_should_pass:
                 assert False
+
+
+class TestLoginRequest:
+    def test_1(self):
+        assert True
